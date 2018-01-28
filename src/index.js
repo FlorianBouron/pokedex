9190bf8fd1a6c987aaa28d26590db5303e6f677a
Florian Bouron <bouron.florian@yahoo.fr>
2018-01-28 12:26:24 +0100

    Add material UI

--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import configureStore from './store'
 import routes from "./routes";
 import registerServiceWorker from './registerServiceWorker';
+import { theme } from './theme';
 import './index.css';
 
 const state = window.__initialState__ || undefined;
 const store = configureStore(state);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
-      {routes(store)}
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider muiTheme={theme}>
+    <Provider store={store}>
+      <BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
+        {routes(store)}
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root'));
 registerServiceWorker();
